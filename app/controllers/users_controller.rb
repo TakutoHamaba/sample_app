7284002471810f38d9106a312f633db6449619f7
EC2 Default User <magicball_3d@yahoo.co.jp>
2019-08-05 12:13:27 +0000

    Add user microposts

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -12,8 +12,7 @@ class UsersController < ApplicationController
   # GET /users/:id
   def show
     @user = User.find(params[:id])
-    # => app/views/users/show.html.erb
-    # debugger
+    @microposts = @user.microposts.paginate(page: params[:page])
   end
 
   # GET /users/new
@@ -72,14 +71,7 @@ class UsersController < ApplicationController
         :password_confirmation)
     end
 
-    # ログイン済みユーザーかどうか確認
-    def logged_in_user
-      unless logged_in?
-        store_location
-        flash[:danger] = "Please log in."
-        redirect_to login_url
-      end
-    end
+    # ログイン済みユーザーかどうか確認→application_controllerに
 
     # 正しいユーザーかどうか確認
     def correct_user
